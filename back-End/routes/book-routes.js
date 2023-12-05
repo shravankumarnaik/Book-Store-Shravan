2ea2a589fd1d066e0da64943dfa372a4213aef88
shravankumarnaik <shravanvd3110@gmail.com>
2023-12-05 16:28:18 +0530

    final

--- back-End/routes/book-routes.js
+++ back-End/routes/book-routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const Book = require("../model/Book")
 const booksController = require("../controllers/books-controller")
 //router get method
-router.get("/books",booksController.getAllBooks)
-router.post("/add",booksController.addBook)
+router.get("/",booksController.getAllBooks)
+router.post("/",booksController.addBook)
 router.get("/:id",booksController.getById);
 router.put("/:id",booksController.updateBook);
 router.delete("/:id",booksController.deleteBook);
